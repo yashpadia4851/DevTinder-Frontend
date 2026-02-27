611cc6e95da7eb07b09c2225539c1a0b9c3748b3
Yash Padia <yashpadia001@gmail.com>
2026-02-27 15:58:31 +0530

    routing added

--- src/App.jsx
+++ src/App.jsx
@@ -1,50 +1,19 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Body from "./Screen/Body/Body";
+import Feed from "./Screen/Body/Feed";
+import Footer from "./Screen/Body/Footer";
+
 function App() {
   return (
     <>
-      <div className="navbar bg-base-300 shadow-sm">
-        <div className="flex-1">
-          <a className="btn btn-ghost text-xl">DevTinder</a>
-        </div>
-        <div className="flex gap-2">
-          {/* <input
-            type="text"
-            placeholder="Search"
-            className="input input-bordered w-24 md:w-auto"
-          /> */}
-          <div className="dropdown dropdown-end mx-5">
-            <div
-              tabIndex={0}
-              role="button"
-              className="btn btn-ghost btn-circle avatar"
-            >
-              <div className="w-10 rounded-full">
-                <img
-                  alt="Tailwind CSS Navbar component"
-                  src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp"
-                />
-              </div>
-            </div>
-            <ul
-              tabIndex="-1"
-              className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
-            >
-              <li>
-                <a className="justify-between">
-                  Profile
-                  <span className="badge">New</span>
-                </a>
-              </li>
-              <li>
-                <a>Settings</a>
-              </li>
-              <li>
-                <a>Logout</a>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </div>
-      <h1 className="text-4xl font-bold bg-amber-400 w-fit">yash</h1>;
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Body />}>
+            <Route path="feed" element={<Feed />} />
+            {/* <Route path="footer" element={<Footer />} /> */}
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
